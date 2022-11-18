e9278290c5f4822f5c26bfe36b48cbea9a184399
Sten Olmre <60060439+stenolmre@users.noreply.github.com>
2022-11-18 14:29:33 +0200

    added load more functionality when scrolling down + modified details sections css

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import useMovies from '../../hooks/useMovies'
+import useOnViewport from '../../hooks/useOnViewport'
 
 import Card from './card/Card'
 
 import styles from './Movies.module.scss'
 
 const Movies = () => {
-  const { movies } = useMovies()
+  const { movies, loadMoreMovies } = useMovies()
   const [show_details, setShowDetails] = useState()
+
+  const last_list_item = useRef()
+  const is_intersection = useOnViewport(last_list_item)
+
+  useEffect(() => {
+    if (is_intersection) {
+      console.log('LOAD MORE')
+      loadMoreMovies()
+    }
+  }, [is_intersection])
 
   return <ul className={styles.movies__list} type="none">
     {
@@ -19,6 +30,7 @@ const Movies = () => {
         setShowDetails={() => setShowDetails(index)}
       />)
     }
+    <li ref={last_list_item}></li>
   </ul>
 }
 
